7e2f96a8bbe63a8f2c69a309cbb1753ff765d96e
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-02 22:18:41 +0530

    Learn Searching the element & more practice on updating the item

---
+++ main.js
@@ -0,0 +1,63 @@
+const users = [
+  { id: 1, name: "Ritesh", age: 20 },
+  { id: 2, name: "Aman", age: 17 },
+  { id: 3, name: "Rahul", age: 22 },
+];
+//function for updating the age of the user
+
+function updateUser(users, id, newAge) {
+  const updatedUsers = [];
+  for (const user of users) {
+    if (user.id === id) {
+      user.age = newAge;
+      updatedUsers.push(user);
+    }
+
+    if (user.id !== id) {
+      updatedUsers.push(user);
+    }
+  }
+  return updatedUsers;
+}
+let updatedUser = [];
+updatedUser = updateUser(users, 3, 30);
+console.log("Updated users list : ", updatedUser);
+
+//function for searching the uaser
+function searchUser(users, keyword) {
+  let searchedUsers = [];
+  for (const user of users) {
+    let name = user.name;
+    let anothername = name.toLowerCase();
+    if (anothername.includes(keyword.toLowerCase())) {
+      searchedUsers.push(user);
+    }
+  }
+  return searchedUsers;
+}
+
+let sUser = [];
+sUser = searchUser(users, "ra");
+console.log("Searched users list : ", sUser);
+
+//function for updating the  user's name
+
+function updateUserName(users, id, newName) {
+  let newArr = [];
+  for (const user of users) {
+    if (user.id === id) {
+      const newUser = { id: user.id, name: newName, age: user.age };
+      newArr.push(newUser);
+    } else {
+      newArr.push(user);
+    }
+  }
+  return newArr;
+}
+ 
+const updateUserList = updateUserName(users, 2, "Rohit");
+for (const user of updateUserList) {
+  console.log(user.name);
+}
+     
+ 
