ef7def3fd898e8c5e25900dbc3f926a9953c5958
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-03 11:46:32 +0530

    Handling Url in Node.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,35 @@
 const http = require("http");
 const fs = require("fs");
- 
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    if(req.url === "/favicon.ico") return res.end();
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    if(err){
-        return res.status(404).end("Error Reading File ")
+    if (err) {
+      return res.status(404).end("Error Reading File ");
     }
-    switch(req.url){
-        case '/': res.end("Home Page");
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Home Page");
         break;
-        case '/about': res.end("I am Ritesh Kumar")
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi, ${username}`);
+        res.end("I am Ritesh Kumar");
         break;
-        case '/contact': res.end("Hey Contact us at: 9719457845");
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("This is result for your quesry :" + search);
         break;
-        default : res.end("404 not Found");
-        break ;
-    } 
- 
+      default:
+        res.end("404 not Found");
+        break;
+    }
   });
-}); 
+});
 myServer.listen(8000, () => {
   console.log("Server Started !");
 });
