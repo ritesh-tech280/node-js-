a1e8e79aee64807e82c29ab5b4521aebf21286b7
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-08 19:52:42 +0530

    Learn : the delete method of REST APi

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const data = require("./MOCK_DATA.json");
-const fs = require('fs')
+let data = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
-app.use(express.urlencoded({ extended : false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("helo ")
+  res.send("helo ");
 });
 
 // app.get("/api/users", (req, res) => {
@@ -32,39 +33,41 @@ app.get("/", (req, res) => {
 //     res.status(200).send(html);
 //   }
 // });
-app.get('/api/users/',(req, res) => {
-  res.json(data)
-})
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = data.find((user) => user.id == id);
-    if (Number.isNaN(id)){
-      return res.status(400).send("Bad Request");
-    } else if (!user) {
-      return res.status(404).send("User not found");
-    } else {
-       res.status(200).send(user);
-    }
-  })
-  .patch((req, res) => {
-    // update the user  
-    res.json({ status: "pending" });
-  })
-  .delete((req,res) => {
-    //deleter the user
-    res.json({status : 'pending'})
-  })
+app.get("/api/users/", (req, res) => {
+  res.json(data);
+});
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) {
+    return res.status(400).send("Bad Request");
+  }
+  const user = data.find((user) => user.id == id);
+   if (!user) {
+    return res.status(404).send("User not found");
+  } else {
+    res.status(200).json(user);
+  }
+});
+app.patch("/api/users/:id", (req, res) => {
+  // update the user
+  res.json({ status: "pending" });
+});
+app.delete("/api/users/:id", (req, res) => {
+  const id =  parseInt(req.params.id);
+  const users = data.filter((user) => user.id !== id);
+ 
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    res.json({ status: "Success", id });
+  });
+});
 
-  app.post('/api/users/', (req,res) => {
-    const body = req.body;
-    data.push({...body, id: data.length + 1});
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(data), (err, data) => {
-
-      res.json({status : 'Success'})
-    })
-  })
+app.post("/api/users/", (req, res) => {
+  const body = req.body;
+  data.push({ ...body, id: data.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err, data) => {
+    res.json({ status: "Success" });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`server running on Port : ${PORT}`);
