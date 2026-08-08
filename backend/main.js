6db4f97fdda08134ba24a178ff99524e6cb56d8d
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-08 12:45:18 +0530

    Learn : Creation of REST APIs and implement get and Post operations

--- backend/main.js
+++ backend/main.js
@@ -1,51 +1,71 @@
 const express = require("express");
 const data = require("./MOCK_DATA.json");
+const fs = require('fs')
 
 const app = express();
 const PORT = 3000;
-app.use(express.json())
+app.use(express.urlencoded({ extended : false }));
 
-app.get('/', (req, res) => {
-  res.send(`Hello this is home page Mr. ${req.query.name}`)
+app.get("/", (req, res) => {
+  res.send("helo ")
+});
+
+// app.get("/api/users", (req, res) => {
+//   const age = Number(req.query.age);
+//   const user = data.filter((user) => user.age == age);
+//   if (!user) {
+//     return res.status(400).send("User not found");
+//   }
+//   const html = user
+//     .map((user) => {
+//       return `<ul> <li>${user.first_name}</li></ul>`;
+//     })
+//     .join("");
+
+//   if (Number.isNaN(age)) {
+//     return res.status(400).send("Bad Request");
+//   }
+
+//   if (user.length === 0) {
+//     return res.status(404).send("User not found ");
+//   } else {
+//     res.status(200).send(html);
+//   }
+// });
+app.get('/api/users/',(req, res) => {
+  res.json(data)
 })
- 
-app.get("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = data.find((user) => user.id == id);
-  if (Number.isNaN(id)) {
-    return res.status(400).send("Bad Request");
-  } else if (!user) {
-    return res.status(404).send("User not found");
-  } else {
-   return  res.status(200).send(user);
-  }
-});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = data.find((user) => user.id == id);
+    if (Number.isNaN(id)){
+      return res.status(400).send("Bad Request");
+    } else if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+       res.status(200).send(user);
+    }
+  })
+  .patch((req, res) => {
+    // update the user  
+    res.json({ status: "pending" });
+  })
+  .delete((req,res) => {
+    //deleter the user
+    res.json({status : 'pending'})
+  })
 
- 
-app.get("/users", (req, res) => {
-  const age = Number(req.query.age);
-  const user = data.filter((user) => user.age == age);
-  if (!user) {
-    return res.status(400).send("User not found");
-  }
-  const html = user.map((user) => {
-      return `<ul> <li>${user.first_name}</li></ul>`;
-    }).join("");
-   
-   if(Number.isNaN(age)) {
-    return res.status(400).send("Bad Request");
-  } 
+  app.post('/api/users/', (req,res) => {
+    const body = req.body;
+    data.push({...body, id: data.length + 1});
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(data), (err, data) => {
 
-  if(user.length === 0) {
-    return res.status(404).send("User not found ");
-  } else {
-    res.status(200).send(html);
-  }
-});
-
+      res.json({status : 'Success'})
+    })
+  })
 
 app.listen(PORT, () => {
   console.log(`server running on Port : ${PORT}`);
 });
-
- 
