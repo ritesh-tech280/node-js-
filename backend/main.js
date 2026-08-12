7653ecbb5a643908b07ae3aa8564ec46ae422b9a
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-12 19:02:44 +0530

    Learn: Using & writting Middleware

--- backend/main.js
+++ backend/main.js
@@ -2,16 +2,33 @@ const express = require("express");
 let data = require("./MOCK_DATA.json");
 const fs = require("fs");
 
-
 const app = express();
 const PORT = 3000;
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json());
+
+//Using Middleware
+app.use((req, res, next) => {
+  console.log("Helllo from middle ware 1");
+  fs.writeFile(
+    "log.txt",
+    `This ${req.method} request come from middleware 1 from ${req.path} `,
+    (err, data) => {
+      next();
+    },
+  );
+});
+// writting middleware 
+const myBlogger = function (req, res, next) {
+  console.log("BLOGGED");
+  next();
+};
+
+app.use(myBlogger);
 
 app.get("/", (req, res) => {
   res.send("helo ");
 });
- 
 
 app.get("/api/users/", (req, res) => {
   res.json(data);
@@ -22,7 +39,7 @@ app.get("/api/users/:id", (req, res) => {
     return res.status(400).send("Bad Request");
   }
   const user = data.find((user) => user.id == id);
-   if (!user) {
+  if (!user) {
     return res.status(404).send("User not found");
   } else {
     res.status(200).json(user);
@@ -30,26 +47,25 @@ app.get("/api/users/:id", (req, res) => {
 });
 app.patch("/api/users/:id", (req, res) => {
   // update the user
-  
+
   const id = Number(req.params.id);
-  const useIndex = data.findIndex((user) => user.id == id)
-  if(Number.isNaN(id)){
-    return res.status(400).send('Bad Request')
+  const useIndex = data.findIndex((user) => user.id == id);
+  if (Number.isNaN(id)) {
+    return res.status(400).send("Bad Request");
   }
-  if(useIndex == -1){
-    return res.status(404).send('User not Found')
+  if (useIndex == -1) {
+    return res.status(404).send("User not Found");
   }
-  data[useIndex] = { ...data[useIndex], ...req.body};
+  data[useIndex] = { ...data[useIndex], ...req.body };
 
-  fs.writeFile('./MOCK_DATA.json', JSON.stringify(data) , (err, data) => {
-
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err, data) => {
     res.json({ status: "success" });
-  })
+  });
 });
 app.delete("/api/users/:id", (req, res) => {
-  const id =  parseInt(req.params.id);
+  const id = parseInt(req.params.id);
   const users = data.filter((user) => user.id !== id);
- 
+
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
     res.json({ status: "Success", id });
   });
