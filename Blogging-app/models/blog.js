aa91ac5379b484d5d3b46f03871ae5f46d942cc8
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-27 19:07:40 +0530

    add Schema of User

---
+++ Blogging-app/models/blog.js
@@ -0,0 +1,25 @@
+const { Schema , model } = require("mongoose")
+
+const userSchema = new Schema({
+     firstName : {
+        type: String,
+        required: true,
+     },
+     email : {
+        type : String ,
+        required : true ,
+        unique : true ,
+     } ,
+     salt  : {
+        type : String ,
+        required : true
+     } ,
+     password : { 
+        type  : String ,
+        required : true
+     }
+} , { timestamps : true })
+
+const User = model('user', userSchema);
+
+module.exports = User  ;
