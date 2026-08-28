dd3d2d352bb8c2e6ddbb7a10b32cbeb3a2f8d8f6
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-08-28 12:42:44 +0530

    Update the user schema

--- Blogging-app/models/user.js
+++ Blogging-app/models/user.js
@@ -1,29 +1,52 @@
-const { Schema , model } = require("mongoose")
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
-const userSchema = new Schema({
-     firstName : {
-        type: String,
-        required: true,
-     },
-     email : {
-        type : String ,
-        required : true ,
-        unique : true ,
-     } ,
-     salt  : {
-        type : String ,
-        required : true
-     } ,
-     password : { 
-        type  : String ,
-        required : true
-     } ,
-     profileUrl : {
-      type : String ,
-      default : '/images/profile.png'
-     }
-} , { timestamps : true })
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileUrl: {
+      type: String,
+      default: "/images/profile.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true },
+);
 
-const User = model('user', userSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified()) return;
 
-module.exports = User  ;
+  const salt = randomBytes(16).toString();
+  const hashpassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+    this.salt = salt ;
+    this.password = hashpassword;
+
+    next()
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
