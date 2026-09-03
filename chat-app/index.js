348e9c293e52328a31daa50a7d96d961e129203d
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-09-03 19:56:06 +0530

    Learn : Websocket Real TIme Messaging

---
+++ chat-app/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const  { Server } = require('socket.io')
+const app = express();
+const server = http.createServer(app);
+
+
+const io = new Server(server); 
+
+app.use(express.static('./public'))
+
+io.on('connection', (socket) => {
+    socket.on('user-message' , message => {
+       io.emit('message' , message);
+    })
+});
+
+app.get('/' , (req,res) => {
+    res.sendFile('./index.html')
+})
+
+
+server.listen(8000, ()=> console.log(`Server running on PORT : 8000`))
