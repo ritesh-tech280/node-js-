425b3f5f35b909d341a792ac3e64d9ef94225419
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-09-04 10:59:10 +0530

    Learn : The concept of STreams

---
+++ streams/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs')
+const app = express()
+const stream = require('node:stream')
+const status = require('express-status-monitor')
+const port = 3000
+
+app.use(status())
+app.get('/', (req, res) => {
+    const stream = fs.createReadStream('./sample.txt', "utf-8")
+    stream.on('data' , (chunk) => res.write(chunk))
+    stream.on('end' , ()=> res.end())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
