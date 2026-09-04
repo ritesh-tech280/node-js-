9849aa600ac559de212d73d2be969066668be329
ritesh-tech280 <riteshkumar01722@gmail.com>
2026-09-04 17:57:25 +0530

    Convert the Zip file

--- streams/index.js
+++ streams/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const fs = require('fs')
 const app = express()
 const stream = require('node:stream')
-const status = require('express-status-monitor')
+const status = require('express-status-monitor');
+const zlib = require('node:zlib')
 const port = 3000
 
+fs.createReadStream('./sample.txt').pipe(zlib.createGzip().pipe(fs.createWriteStream('./sample.zip')))
 app.use(status())
 app.get('/', (req, res) => {
     const stream = fs.createReadStream('./sample.txt', "utf-8")
